a4fb441643e45389bb534af8e02f92ffe1c60556
Garrett Thompson <gtcruz.gt@gmail.com>
2019-07-11 04:41:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,8 @@ def badge_maker(name)
   "Hello, my name is #{name}"
 end
 
-list_speakers = [Edsger, Ada, Charles, Alan, Grace,
-                 Linus, Matz]
+list_speakers = ["Edsger", "Ada", "Charles", "Alan",
+                "Grace", "Linus", "Matz"]
 def batch_badge_creator(speakers)
   speakers.each do |who|
     badge_maker(who)
